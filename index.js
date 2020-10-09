b80a14220da374c3c92586b23a28686ff0abc9b6
Denisson Gomes <dnsgom@gmail.com>
2020-10-09 20:26:33 -0300

    Fix __dirname typo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path')
 const port =  3000
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(__disrname, 'views'))
+app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get("/", (req,res)=>{

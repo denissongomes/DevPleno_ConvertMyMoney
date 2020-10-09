23c60fa4321e4398fdc364aaf8d12043b6deaa1b
Denisson Gomes <dnsgom@gmail.com>
2020-10-09 20:22:27 -0300

    Initial code for server conection

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port =  3000
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__disrname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get("/", (req,res)=>{
+    res.render('home')
+})
+
+
+app.listen(port, err=>{
+    if(err){
+        console.log('Não foi possível conectar ao servidor')
+
+    } else {
+        console.log('ConvertMyMoney está online')
+        
+    }
+})
